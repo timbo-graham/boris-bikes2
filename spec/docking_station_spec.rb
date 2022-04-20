a302b24089b5823738581efcc9890a5ce3ed7c3d
timbo-graham <tmgraham29@gmail.com>
2022-04-20 16:32:59 +0100

    Passed release_to test by creating the release_bike class method

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,7 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-  it "releases bike" do
-    expect(DockingStation).to respond_to(release_bike)
-  end
+
+  it { is_expected.to respond_to :release_bike }
+
 end
