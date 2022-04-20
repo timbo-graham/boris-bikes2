979cb974eec29caffd64518f5ed5c58c05f1bcfa
Anish <anishkakaiya@outlook.com>
2022-04-20 17:42:54 +0100

    rspec refactor code

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,10 +5,8 @@ describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
-  describe ".release_bike" do
-    it "Gets a bike and checks it's working" do
-      bike = subject.release_bike
-      expect(bike.working?).to eq(true)
-    end
+  it "Gets a bike and checks it's working" do
+    bike = subject.release_bike
+    expect(bike.working?).to eq(true)
   end
 end
