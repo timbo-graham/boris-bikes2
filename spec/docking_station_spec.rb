1a1bfc781c1b1b0ccb28db3767212eeddc6aed1f
timbo-graham <tmgraham29@gmail.com>
2022-04-20 17:38:50 +0100

    Added tests for getting a bike, and checking it's working. Passed them

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,7 +1,14 @@
 require 'docking_station.rb'
+require 'bike.rb'
 
 describe DockingStation do
 
   it { is_expected.to respond_to :release_bike }
 
+  describe ".release_bike" do
+    it "Gets a bike and checks it's working" do
+      bike = subject.release_bike
+      expect(bike.working?).to eq(true)
+    end
+  end
 end
