1f3dc4b82e3967895ee06c77fa1290ec2b95157f
timbo-graham <tmgraham29@gmail.com>
2022-04-21 15:07:00 +0100

    Added tests for docking and retrieving bikes, wrote according methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,8 +2,28 @@ require_relative "bike.rb"
 
 class DockingStation
 
+  attr_reader :bikes
+
+  def initialize
+
+    @bikes = []
+
+  end
+  
   def release_bike
-    Bike.new
+
+    if @bikes.empty?
+      raise StandardError
+    end
+
+  end
+
+
+  def dock_bike(bike)
+    
+    bike = Bike.new
+    @bikes << bike
+
   end
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,13 +3,29 @@ require 'bike.rb'
 
 describe DockingStation do
 
+  let(:docking_station) { DockingStation.new }
+  let(:bike) { Bike.new }
+
   it { is_expected.to respond_to :release_bike }
 
   it "Gets a bike and checks it's working" do
-    bike = subject.release_bike
+    #bike = subject.release_bike
     expect(bike.working?).to eq(true)
+
+  end
   
   it "docks bike" do
-    expect(subject.dock_bike).to eq(true)
+
+    #bike = Bike.new
+    expect(subject.dock_bike(bike))
+
   end
+
+  it "see whether there are any docked bikes" do
+
+    empty_docking_station = DockingStation.new
+    expect { subject.release_bike }.to raise_error(StandardError)
+
+  end
+
 end
